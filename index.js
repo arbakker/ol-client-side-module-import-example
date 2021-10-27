924cae7448185183f3568fe051adad7e75ebc107
Anton Bakker <anton.bakker@kadaster.nl>
2021-10-27 11:54:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import { default as ImageLayer } from 'https://esm.run/ol@6.9.0/src/layer/Image';
 import { default as ImageWMS } from 'https://esm.run/ol@6.9.0/src/source/ImageWMS';
-import { default as olMap } from 'https://esm.run/ol@6.9.0/src/Map';
+import { default as Map } from 'https://esm.run/ol@6.9.0/src/Map';
 import { default as View } from 'https://esm.run/ol@6.9.0/src/View';
 import { default as TileLayer } from 'https://esm.run/ol@6.9.0/src/layer/Tile';
 import { default as OSM } from 'https://esm.run/ol@6.9.0/src/source/OSM';
 
-console.log(olMap)
 const osmSource = new OSM()
 const layers = [
     new TileLayer({
@@ -21,11 +20,11 @@ const layers = [
         }),
     }),
 ];
-const test = new olMap({})
-test.setTarget(document.getElementById('map'))
-test.setLayers(layers)
-// somehow promise is expected instead of view object
-const myPromise = new Promise((resolve, reject) => {
+const map = new Map({})
+map.setTarget(document.getElementById('map'))
+map.setLayers(layers)
+// somehow promise is expected instead of view object https://openlayers.org/en/latest/apidoc/module-ol_Map-Map.html#setView
+const viewOptions = new Promise((resolve, reject) => {
     resolve(
         {
             center: [-10997148, 4569099],
@@ -33,4 +32,4 @@ const myPromise = new Promise((resolve, reject) => {
         }
     );
 });
-test.setView(myPromise)
+map.setView(viewOptions)
